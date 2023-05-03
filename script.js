b1c79d6d5ae500d738806004c7a47acf01a879f2
Arjayieeee <federioarjay16@gmail.com>
2023-05-03 11:28:56 +0800

    updated

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const toggleMenu = document.querySelector(".toggle__menu");
-const headNav = document.querySelector(".header__nav ul");
-const headear = document.querySelector(".header");
+const headNav = document.querySelector(".header__nav2");
+const headear = document.querySelector(".header__nav2");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headNav.classList.toggle("open");
-  headers.style="border-bottom:5px solid #ffffff;";
+  headNav.classList.toggle("open");
+  headers.style="border-bottom:5px solid gray;";
 });
